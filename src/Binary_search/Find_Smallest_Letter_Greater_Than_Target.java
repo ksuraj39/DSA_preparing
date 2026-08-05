41636cfbfc73ee174362e314e298be23c638c6d6
ksuraj39 <ksuraj87945@gmail.com>
2026-08-05 10:42:43 +0530

    again

--- src/Binary_search/Find_Smallest_Letter_Greater_Than_Target.java
+++ src/Binary_search/Find_Smallest_Letter_Greater_Than_Target.java
@@ -1,9 +1,28 @@
 package Binary_search;
 
+//     Question name : Find Smallest Letter Greater Than Target
+//     Leetcode Number : 744
+//     Level : Easy
+/*
+    Constraints:
+
+        2 <= letters.length <= 104
+        letters[i] is a lowercase English letter.
+        letters is sorted in non-decreasing order.
+        letters contains at least two different characters.
+        target is a lowercase English letter.
+
+ */
+
 public class Find_Smallest_Letter_Greater_Than_Target {
     public static void main(String[] args) {
-
+        char[] letter = {'c','f','j'};
+        char target = 'c';
+        System.out.println(nextGreatestLetter(letter,target));
     }
+    /*      Solution : using Binary search (Upper bound concept)
+            TC -- O(log n) ,  SC -- O(1)
+     */
     public static char nextGreatestLetter(char[] letters, char target) {
         int n = letters.length;
         int low = 0,high = n-1;
