b02303982f9bea755ba4a9c2fdc146f31b8ff4ca
ksuraj39 <ksuraj87945@gmail.com>
2026-08-06 06:43:42 +0530

    domino Piling

---
+++ src/code_forces/Dominopiling.java
@@ -0,0 +1,13 @@
+package code_forces;
+
+import java.util.*;
+
+public class Dominopiling {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+        System.out.println((m*n)/2);
+    }
+}
