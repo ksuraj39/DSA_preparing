9f19c9ea396a8c78d4fb6866202251f9a5e2bd8c
ksuraj39 <ksuraj87945@gmail.com>
2026-08-22 07:25:51 +0530

    same patterns as we say in codeforces april food

---
+++ src/code_forces/Pathern.java
@@ -0,0 +1,13 @@
+package code_forces;
+
+
+public class Pathern {
+    public static void main(String[] args) {
+        for (char i = 'A';i <= 'C';i++) {
+            for (int j = 1; j <= 5 ; j++) {
+                System.out.print(i+""+j);
+            }
+            System.out.println();
+        }
+    }
+}
